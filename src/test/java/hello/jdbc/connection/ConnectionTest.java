4462b2010722a8b2c5be58ebd5a03e3bc11dfec8
홍성민 <bigbosshong1101@gmail.com>
2025-01-02 22:54:08 +0900

    DataSource 예제2 - 커넥션 풀

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,9 +1,11 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,12 +14,6 @@ import static hello.jdbc.connection.ConnectionConst.*;
 @Slf4j
 public class ConnectionTest {
 
-    private static void useDataSource(final DriverManagerDataSource dataSource) throws SQLException {
-        final Connection con1 = dataSource.getConnection();
-        final Connection con2 = dataSource.getConnection();
-        log.info("connection={}, class={}", con1, con1.getClass());
-        log.info("connection={}, class={}", con2, con2.getClass());
-    }
 
     @Test
     void driveManager() throws SQLException {
@@ -35,4 +31,28 @@ public class ConnectionTest {
 
         useDataSource(dataSource);
     }
+
+
+    private static void useDataSource(final DataSource dataSource) throws SQLException {
+        final Connection con1 = dataSource.getConnection();
+        final Connection con2 = dataSource.getConnection();
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링
+        final HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000 );
+
+    }
+
 }
