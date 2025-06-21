425aa902c15fd2fdcdfb47e70632b737172ca5d9
홍성민 <bigbosshong1101@gmail.com>
2025-06-21 13:14:16 +0900

    체크 예외와 인터페이스

--- src/main/java/hello/jdbc/repository/MemberRepositoryV3.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV3.java
@@ -17,7 +17,7 @@ import java.util.NoSuchElementException;
  */
 @Slf4j
 @RequiredArgsConstructor
-public class MemberRepositoryV3 {
+public class MemberRepositoryV3 implements MemberRepositoryEx {
 
     private final DataSource dataSource;
 
